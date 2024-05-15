7ad27f66e0535dde911a0cef9a290f1edd1e4faa
Ville Saloranta <SRanta.Ville@gmail.com>
2024-05-15 14:54:36 +0300

    Additions for part 5

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,7 +5,7 @@ function menuFunctionality()
     const menuNav       = document.querySelector(".menu-nav");
     const menuBranding  = document.querySelector(".menu-branding");
 
-    const navItems      = document.querySelectorAll(".nav-items");
+    const navItems      = document.querySelectorAll(".nav-item");
 
     //Initial state of the menu
     let showMenu = false;
